ee804d2dfae3cf346d27f8d6589d19c3315afd3c
Barioso <diogo.freire1020@gmail.com>
2024-08-20 18:14:47 -0300

    finalização do projeto: implementando funções de autenticação e mudanças nas páginas html para melhor enquadrar as funções.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 //é preciso usar recursos já prontos que impeçam a reinvenção da roda.
 import express from 'express';
+import autenticar from './seguranca/autenticar.js';
+import { verificarAutenticacao, logout } from './seguranca/autenticar.js';
+import session from 'express-session';
 
 const host = '0.0.0.0'; //Todas as interfaces de rede disponíveis
 //em um computador há diversos programas sendo executados
@@ -7,8 +10,30 @@ const host = '0.0.0.0'; //Todas as interfaces de rede disponíveis
 const porta = 3000;
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+
+//aqui está sendo configurado o uso da biblioteca session
+app.use(session({
+    secret: 'segredo',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 15
+    }
+}))
+
 app.use(express.static('./publico'));
 
+//endpoint http://localhost:3000/login
+app.get('/login',(requisicao, resposta) => {
+    resposta.redirect('/login.html');
+});
+
+app.get('/logout', logout)
+
+app.post('/login', autenticar);
+
+app.use(verificarAutenticacao, express.static('./privado'));
 
 
 //listen = escutar pro requisições dos usuários
